7903e2c095224c871008c198873ede0ca17be267
ByeonWoojin <162543327+ByeonWoojin@users.noreply.github.com>
2024-04-30 19:22:24 +0900

    Add files via upload
    event

--- App.js
+++ App.js
@@ -3,18 +3,21 @@ import './App.css';
 function Header(props){     //props를 이용해서 속성? 약간 매개변수로 생각하면 괜찮을 것 같음
   console.log('props', props, props.title);
   return       <header>
-  <h1><a href="/">WEB</a></h1>
+  <h1><a href="/" onClick={function(event){
+    event.preventDefault();
+    props.onChangeMode();
+  }}>WEB</a></h1>
 </header>
 }
 function Nav(props){
-  const lis = [   
-  <li><a href = "/read/1">html</a></li>,
-  <li><a href = "/read/2">css</a></li>,
-  <li><a href = "/read/3">Js</a></li>
-]
+  const lis = []
 for(let i = 0; i<props.topics.length; i++){
   let t = props.topics[i];
-  lis.push(<li key={t.id}><a href = {'/read/'+t.id}>{t.title}</a></li>)      //{}로 묶어서 동적으로 만들 수 있음
+  lis.push(<li key={t.id}>
+    <a id={t.id} href = {'/read/'+t.id} onClick={event=>{
+      event.preventDefault();
+      props.onChangeMode(event.target.id);
+    }}>{t.title}</a></li>)      //{}로 묶어서 동적으로 만들 수 있음
 }   //리엑트는 자동으로 생성한 태그의 경우 리엑트가 이 태그들을 추적해야되는데 이 근거로써 우리가 li key 를 부여해서 정확한 동작하게 구현
  return       <nav>
  <ol>
@@ -37,8 +40,12 @@ function App() {
   ];
   return (
     <div>
-      <Header title = "REACT"></Header>
-      <Nav topics ={topics}></Nav>
+      <Header title = "REACT" onChangeMode={()=>{
+        alert('Header');
+      }}></Header>
+      <Nav topics ={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
       <Article title = "Welcome" body = "Hello,Web"></Article>
       <Article title = "Hi" body = "Wow React"></Article>
 
